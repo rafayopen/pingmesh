f5477329c28dfcddbae14fdc32dc9c4959809d93
John Dilley <jad@rafay.co>
2019-07-29 12:50:39 -0400

    Add server function to start HTTP server

---
+++ pkg/server/server.go
@@ -0,0 +1,35 @@
+// server implements the web server
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// StartServer runs a web server to listen on the given port.  It never returns, so
+// invoke it with go StartServer(yourPort, routes).  Handlers and application state are
+// set up separately.
+func StartServer(port int) error {
+	max := 5 // 5 tries = 15 seconds (linear backoff -- 5th triangular number)
+
+	addr := fmt.Sprintf(":%d", port)
+	log.Println("starting server on port", port)
+
+	// The ListenAndServe call should not return.  If it does the address may be in use
+	// from an instance that just exited; if so retry a few times below.
+	err := http.ListenAndServe(addr, nil)
+
+	tries := 0
+	for tries < max {
+		tries++
+		// sleep a little while longer each time through the loop
+		log.Println(err, "sleep", tries)
+		time.Sleep(time.Duration(tries) * time.Second)
+		// now try again ... it may take a while for a previous instance to exit
+		err = http.ListenAndServe(addr, nil)
+	}
+
+	return err
+}
