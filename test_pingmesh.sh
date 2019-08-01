647065d60d5ddef518831219ba739b31dc39b8c5
John Dilley <jad@rafay.co>
2019-08-01 10:47:45 -0400

    Create multiple instances to test many-ping case

--- test_pingmesh.sh
+++ test_pingmesh.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
-PM="cmd/pingmesh/pingmesh -v"
+PM="cmd/pingmesh/pingmesh"
+usage="Usage: $0 [number of instances]"
 
 echo try one
-$PM -s 8080 -n 5 -d 2 http://localhost:8080/v1/ping
-[ $# -eq 0 ] && exit 0
+$PM -v -s 8080 -n 5 -d 1 http://localhost:8080/v1/ping#Charlevoix,MI
+code=$?
+if [ $code -ne 0 ] ; then
+    echo $PM exited with code $code
+    echo $usage
+    exit $code
+fi
 
-echo try two
-####
-# Run a pair of standalone instances that talk to each other locally and
-# report metrics into cloudwatch
-$PM -s 8080 -c -n 60 -d 10 http://127.0.0.1:8081/v1/ping &
-$PM -s 8081 -c -n 60 -d 10 http://127.0.0.1:8080/v1/ping http://127.0.0.1:8081/v1/ping &
+echo
+if [ "$1" -gt 0 ] ; then
+    min=8081
+    max=$((8080 + $1))
+    echo try N=$1 instances from $min to $max
+else
+    echo $usage
+    exit 1
+fi
+
+urls=""
+for i in $(seq $min $max) ; do
+    urls="$urls http://127.0.0.1:$i/v1/ping"
+    $PM -s $i -c -n 60 -d 10 $urls &
+done
 
 wait
